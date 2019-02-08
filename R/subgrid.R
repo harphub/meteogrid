0818f75ecdf2abecc668f155850ea2eb556c3dd8
Alex Deckmyn <alex.deckmyn@gmail.com>
2019-02-08 23:13:30 +0100

    fix subgrid()

--- R/subgrid.R
+++ R/subgrid.R
@@ -15,29 +15,40 @@ subgrid <- function(geo, x1, x2, y1, y2, reso=1) {
   else if (inherits(geo,"geodomain")) gdomain <- geo
   else stop("subgrid requires a geofield or geodomain as input.")
 
-  xsub <- seq(x1,x2,by=reso)
-  ysub <- seq(y1,y2,by=reso)
-  subnx <- length(xsub)
-  subny <- length(ysub)
+  xsub <- seq(x1, x2, by=reso)
+  ysub <- seq(y1, y2, by=reso)
+  nx <- length(xsub)
+  ny <- length(ysub)
 
-  newlalo <- DomainPoints(geo,"lalo")
-  newlalo$lon <- newlalo$lon[xsub,ysub]
-  newlalo$lat <- newlalo$lat[xsub,ysub]
+  gxy <- DomainPoints(geo, "xy")
+  clonlat <- as.numeric(project(x = (gxy$x[xsub[1], ysub[1]] + gxy$x[xsub[nx], ysub[ny]])/2,
+				y = (gxy$y[xsub[1], ysub[1]] + gxy$y[xsub[nx], ysub[ny]])/2,
+				proj = gdomain$projection,
+				inv = TRUE))
+  if (is.null(gdomain$dx)) {
+    dx <- (gxy$x[xsub[nx], ysub[ny]] - gxy$x[xsub[1], ysub[1]]) / (nx-1)
+  } else {
+    dx <- gdomain$dx * reso
+  }
+  if (is.null(gdomain$dy)) {
+    dy <- (gxy$y[xsub[nx], ysub[ny]] - gxy$y[xsub[1], ysub[1]]) / (ny-1)
+  } else {
+    dy <- gdomain$dy * reso
+  }
 
-  gdomain$SW <- c(newlalo$lon[1,1],newlalo$lat[1,1])
-  gdomain$NE <- c(newlalo$lon[subnx,subny],newlalo$lat[subnx,subny])
+  newdomain <- list(projection=gdomain$projection, 
+                    nx = nx, ny = ny,
+		    dx = dx, dy = dy,
+		    clonlat = clonlat)
+  class(newdomain) <- "geodomain"
 
-  gdomain$nx <- length(xsub)
-  gdomain$ny <- length(ysub)
-  gdomain$dx <- gdomain$dx * reso
-  gdomain$dy <- gdomain$dy * reso
-  cxy <- (project(gdomain$NE, proj=gdomain$projection) + project(gdomain$SW, proj=gdomain$projection)) / 2
   # use as.numeric()' to coerce a data.frame to a vector:
-  gdomain$clonlat <- as.numeric(project(cxy, proj=gdomain$projection, inv=TRUE))
-  if (inherits(geo,"geofield")) {
-    as.geofield(geo[xsub,ysub], domain=gdomain, time=attr(geo,"time"),
-             info=c(attr(geo,"info"), extra="SUBFIELD"))
-  } else gdomain
+  if (inherits(geo, "geofield")) {
+    as.geofield(geo[xsub,ysub], domain=newdomain, time=attr(geo,"time"),
+                info=c(attr(geo,"info"), extra="SUBFIELD"))
+  } else {
+    newdomain
+  }
 }
 
 ################
@@ -45,7 +56,7 @@ subgrid <- function(geo, x1, x2, y1, y2, reso=1) {
 zoomgrid <- function(geo, x, y, zoom=50){
   if (inherits(geo,"geofield")) gdomain <- attr(geo,"domain")
   else if (inherits(geo,"geodomain")) gdomain <- geo
-  else stop("subgrid requires a geofield or geodomain as input.")
+  else stop("zoomgrid requires a geofield or geodomain as input.")
 
   if (x-zoom < 1) xmin <- 1
   else if (x+zoom > gdomain$nx) xmin <- gdomain$nx-2*zoom
@@ -54,10 +65,10 @@ zoomgrid <- function(geo, x, y, zoom=50){
   if (y-zoom < 1) ymin <- 1
   else if (y+zoom > gdomain$ny) ymin <- gdomain$ny-2*zoom
   else ymin <- y-zoom
-  xmax <- xmin+2*zoom
-  ymax <- ymin+2*zoom
+  xmax <- xmin + 2*zoom
+  ymax <- ymin + 2*zoom
 
-  subgrid(geo,xmin,xmax,ymin,ymax,reso=1)
+  subgrid(geo, xmin, xmax, ymin, ymax, reso=1)
 }
 
 
